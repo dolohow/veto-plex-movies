980a11c00dfeb61a16fad65de5dd9db9d82470f5
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2021-01-30 23:24:42 +0100

    logging: Log line number

--- poll.py
+++ poll.py
@@ -12,7 +12,7 @@ from telegram.ext import (Updater, CommandHandler, CallbackQueryHandler, Convers
 CONFIG = configparser.ConfigParser()
 CONFIG.read("poll.ini")
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format='%(asctime)s %(lineno)d - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
